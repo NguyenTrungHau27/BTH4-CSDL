971dbeb759194fa6e8cb7924c11d6bea23f23978
hau <1551010032hau@gmail.com>
2017-09-27 11:09:16 +0700

    BTTH4

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace BaiTH4
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rd2 = new System.Windows.Forms.RadioButton();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rd1 = new System.Windows.Forms.RadioButton();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.txtDatabase);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtServerName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(334, 369);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kết nối đến server";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rd2);
+            this.groupBox2.Controls.Add(this.txtPassword);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.rd1);
+            this.groupBox2.Controls.Add(this.txtUserName);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(6, 125);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(302, 175);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chứng thực";
+            // 
+            // rd2
+            // 
+            this.rd2.AutoSize = true;
+            this.rd2.Location = new System.Drawing.Point(22, 70);
+            this.rd2.Name = "rd2";
+            this.rd2.Size = new System.Drawing.Size(226, 24);
+            this.rd2.TabIndex = 0;
+            this.rd2.Text = "SQL Server Authentication";
+            this.rd2.UseVisualStyleBackColor = true;
+            this.rd2.CheckedChanged += new System.EventHandler(this.rd2_CheckedChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(133, 131);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(163, 27);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Password";
+            // 
+            // rd1
+            // 
+            this.rd1.AutoSize = true;
+            this.rd1.Checked = true;
+            this.rd1.Location = new System.Drawing.Point(22, 40);
+            this.rd1.Name = "rd1";
+            this.rd1.Size = new System.Drawing.Size(197, 24);
+            this.rd1.TabIndex = 0;
+            this.rd1.TabStop = true;
+            this.rd1.Text = "Window Authentication";
+            this.rd1.UseVisualStyleBackColor = true;
+            this.rd1.CheckedChanged += new System.EventHandler(this.rd1_CheckedChanged);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(133, 100);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(163, 27);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "User name";
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(139, 77);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(169, 27);
+            this.txtDatabase.TabIndex = 1;
+            this.txtDatabase.Text = "QLBanHang";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Database name";
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.Location = new System.Drawing.Point(139, 44);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(169, 27);
+            this.txtServerName.TabIndex = 1;
+            this.txtServerName.Text = "(local)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(150, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 406);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmLogin";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rd2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rd1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtServerName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ frmLogin.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BaiTH4
+{
+    public partial class frmLogin : Form
+    {
+        private SqlConnection nv = null;
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            nv = new SqlConnection();
+        }
+        public void connect(string cnstr)
+        {
+            try
+            {
+                if (nv != null)
+                {
+                    nv.ConnectionString = cnstr;
+                    nv.Open();
+                    MessageBox.Show("Ket noi thanh cong");
+
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show("Cung cap co so du lieu", ex.Message);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Loi cap do", ex.Message);
+            }
+        }
+        public void disconnect()
+        {
+            nv.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string server, database;
+            server = txtServerName.Text;
+            database = txtDatabase.Text;
+            string cnstr = "Server =";
+            cnstr += server.Trim() + "; Database = " + database+"; ";
+            if (rd1.Checked)
+            {
+                cnstr += "Integrated Security = true;";
+            }
+            else
+            {
+                cnstr += "User = " + txtUserName.Text + "; Password = " + txtPassword.Text + ";";
+            }
+           
+            connect(cnstr);
+        }
+
+        private void rd1_CheckedChanged(object sender, EventArgs e)
+        {
+            txtUserName.Enabled = false;
+            txtPassword.Enabled = false;
+        }
+
+        private void rd2_CheckedChanged(object sender, EventArgs e)
+        {
+            txtUserName.Enabled = true;
+            txtPassword.Enabled = true;
+        }
+
+        
+
+      
+    }
+}
